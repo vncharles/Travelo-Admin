810da92bf66e78dec901dc88f15668c4eb0b52b3
vncharles <quoctrong.13022002@gmail.com>
2023-11-20 19:40:05 +0700

    manage staff

---
+++ JS/SaveStaff.js
@@ -0,0 +1,104 @@
+const dataa = JSON.parse(localStorage.getItem("data"));
+const token = dataa.token;
+
+// Function to extract parameters from the URL
+const getParameterByName = (name, url) => {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+// Function to fetch data and populate the form
+const fetchStaffData = async () => {
+  const staffId = getParameterByName("staff_id");
+
+  if (staffId) {
+    try {
+      const response = await fetch(`http://localhost:8084/staff/${staffId}`);
+      if (response.ok) {
+        const data = await response.json();
+        populateForm(data);
+      } else {
+        console.error("Failed to fetch staff data");
+      }
+    } catch (error) {
+      console.error("Error fetching staff data:", error);
+    }
+  }
+};
+
+const getGenderString = (gender) => {
+  return gender ? "male" : "female";
+};
+
+// Function to populate the form fields with the fetched data
+const populateForm = (data) => {
+  document.getElementById("staffName").value = data.name || "";
+  document.getElementById("staffEmail").value = data.email || "";
+  document.getElementById("staffPhone").value = data.phone || "";
+  document.getElementById("staffIDCard").value = data.personId || "";
+  document.getElementById("staffAddress").value = data.address || "";
+  document.getElementById("staffGender").value = getGenderString(data.gender);
+  document.getElementById("staffDOB").value = data.birthday || "";
+};
+
+// Fetch data when the page loads
+window.addEventListener("DOMContentLoaded", () => {
+  fetchStaffData();
+});
+
+// Function to get form data
+const getFormData = () => {
+  return {
+    name: document.getElementById("staffName").value,
+    email: document.getElementById("staffEmail").value,
+    phone: document.getElementById("staffPhone").value,
+    personId: document.getElementById("staffIDCard").value,
+    address: document.getElementById("staffAddress").value,
+    gender: document.getElementById("staffGender").value === "male", // assuming the value 'male' indicates 'Nam'
+    birthday: document.getElementById("staffDOB").value,
+  };
+};
+
+// Function to handle form submission
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const staffId = getParameterByName("staff_id");
+  const formData = getFormData();
+
+  try {
+    let url = "http://localhost:8084/staff";
+    let method = "POST";
+
+    if (staffId) {
+      url += `/${staffId}`;
+      method = "PUT";
+    }
+
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      alert("Lưu thành công!");
+      window.location.href = "../HTML/Staff.html";
+    } else {
+      alert("Không thể lưu được!");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Có lỗi xảy ra khi lưu!");
+  }
+};
+
+// Event listener for form submission
+document.getElementById("createStaffForm").addEventListener("submit", handleSubmit);
--- JS/Staff.js
+++ JS/Staff.js
@@ -57,122 +57,53 @@ const renderOrders = async () => {
       `;
       tbody.appendChild(tr);
 
-      // // Add event listener for "Sửa" button
-      // const btnEdit = tr.querySelector(".btnEdit");
-      // btnEdit.addEventListener("click", () => {
-      //   // Get the fields to be edited
-      //   const numberPhoneTd = tr.querySelector(".numberPhone");
-      //   const nameReceiverTd = tr.querySelector(".nameReceiver");
-      //   const addressTd = tr.querySelector(".address");
+      // Adding event listener to each "Edit" button
+      const editButton = tr.querySelector(".btn-edit");
+      editButton.addEventListener("click", () => {
+        const staffId = staff.id;
+        // Redirect to form-staff.html with staff_id parameter
+        window.location.href = `form-staff.html?staff_id=${staffId}`;
+      });
 
-      //   // Replace the fields with input fields
-      //   numberPhoneTd.innerHTML = `
-      //     <form>
-      //       <input type="text" id="inputNb" value="${order.numberPhone}">
-      //     </form>
-      //   `;
-      //   const inputNb = numberPhoneTd.querySelector("#inputNb");
-
-      //   nameReceiverTd.innerHTML = `
-      //     <form>
-      //       <input type="text" id="inputName" value="${order.nameReceiver}">
-      //     </form>
-      //   `;
-      //   const inputName = nameReceiverTd.querySelector("#inputName");
-
-      //   addressTd.innerHTML = `
-      //     <form>
-      //       <input type="text" id="inputAdd" value="${order.address}">
-      //     </form>
-      //   `;
-      //   const inputAdd = addressTd.querySelector("#inputAdd");
-
-      //   // Change the button to "Lưu"
-      //   btnEdit.textContent = "Lưu";
-      //   btnEdit.dataset.flag = "save";
-
-      //   // Get the "Lưu" button
-      //   const btnSave = tr.querySelector(".btnEdit[data-flag='save']");
-
-      //   // Add event listener for "Lưu" button
-      //   btnSave.addEventListener("click", async () => {
-      //     // Get the values of the inputs
-      //     const newNumberPhone = inputNb.value;
-      //     const newNameReceiver = inputName.value;
-      //     const newAddress = inputAdd.value;
-
-      //     // Call the API to update the order
-      //     const response = await fetch(
-      //       `http://localhost:8080/api/order/update?id=${order.id}`,
-      //       {
-      //         method: "PUT",
-      //         headers: {
-      //           Authorization: `Bearer ${token}`,
-      //           "Content-Type": "application/json",
-      //         },
-      //         body: JSON.stringify({
-      //           numberPhone: newNumberPhone,
-      //           nameReceiver: newNameReceiver,
-      //           address: newAddress,
-      //         }),
-      //       }
-      //     );
-
-      //     // If the update is successful, reload
-
-      //     // If the update is successful, reload the orders
-      //     if (response.ok) {
-      //       window.location.reload();
-      //     }
-      //   });
-      // });
+      // Adding event listener to each "Delete" button
+      const deleteButton = tr.querySelector(".btn-delete");
+      deleteButton.addEventListener("click", () => {
+        const staffId = staff.id;
+        const confirmation = confirm("Bạn có chắc muốn xoá nhân viên này không?");
+        if (confirmation) {
+          fetch(`http://localhost:8084/staff/${staffId}`, {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+            .then((response) => {
+              if (response.status === 200) {
+                alert("Xoá thành công");
+                location.reload(); // Reload the page after successful deletion
+              } else {
+                throw new Error("Delete request failed");
+              }
+            })
+            .catch((error) => {
+              console.error("Delete request error:", error);
+              alert("Xoá không thành công");
+            });
+        }
+      });
     });
   } catch (error) {
     console.error(error);
   }
 };
 
-//===
-setTimeout(() => {
-  const buttons = document.querySelectorAll(".btnDetail");
-  buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      const orderId = event.target.dataset.id;
-      fetch(`http://localhost:8080/api/order/1`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.items);
-          items = data.items;
-          const tableBody = document.querySelector(".childTable");
-          const Body = document.querySelector(".bodyChild");
-          Body.innerHTML = "";
-          items.map((item) => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-            <td>${item.id}</td>
-            <td>${item.product.name}</td>
-            <td>${item.quantity}</td>
-            <td>${item.product.price}</td>
-            <td>${item.price}</td>
-           
-          `;
-            Body.appendChild(row);
-          });
-        });
-    });
-  });
-}, 1000);
-//name
-const user = document.querySelector("#userName");
-user.innerText = dataa.fullName;
-
 const logout = document.querySelector("#logout");
 logout.addEventListener("click", () => {
   localStorage.removeItem("data");
   localStorage.removeItem("token");
   window.location.href = "/HTML/login.html";
+});
+
+document.getElementById("add-product-btn").addEventListener("click", function() {
+  window.location.href = "form-staff.html";
 });
